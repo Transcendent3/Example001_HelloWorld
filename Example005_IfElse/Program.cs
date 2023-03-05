cb4345c0358c742886f8d1d76cbab0fa8f4b6304
Nikolai <dolinnikolai@gmail.com>
2023-03-05 14:46:17 +0300

    Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("What is u name?");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Yeah, Its Masha!!!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
